d1ee689e401350fb7fec9a9577bf155db729fe64
Hongnuri <hongnuri0427@gmail.com>
2020-11-13 18:22:29 +0900

    K05Javascript

--- WebContent/200/examples/part1/004.js
+++
@@ -1,4 +0,0 @@
-console.log();
-var a = 5;
-var b = 2;
-console.log(a + b);
--- WebContent/200/examples/part1/006-2.js
+++
@@ -1,7 +0,0 @@
-funtion greeting() {
-    "hello"
-    "chloe" + 3
-    greeting()
-}
-
-// greeting(if(true) { console.log("It is not acceptable") }) 오류 
